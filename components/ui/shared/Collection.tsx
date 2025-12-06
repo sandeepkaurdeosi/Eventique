b27b89ae946d01e5af9333ca3a7d7e5f495bec63
sandeepkaurdeosi <sandeepdeosi7@gmail.com>
2025-12-06 13:21:24 +0530

    just updated

--- components/ui/shared/Collection.tsx
+++ components/ui/shared/Collection.tsx
@@ -11,6 +11,7 @@ type CollectionProps = {
   page: number | string,
   totalPages?: number,
   urlParamName?: string,
+  userId?: string;
   collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
 }
 
